ef78f66a765ebf01dbdb2ee37d0f57a3277609f0
Mahmoud Safan <mahmoudsafan240@gmail.com>
2022-08-23 20:00:19 +0200

    frist node application

---
+++ async/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+  res.end('hello world after saving');
+});
+
+server.listen(port, hostname, ()=>{
+  console.log(`server is runniong at http://${hostname}:${port}`)
+});
+
+
